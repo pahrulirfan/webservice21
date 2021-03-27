d439e02f338ae5eb4976d33fe0019e7c00fcc7db
Pahrul Irfan <ip.5090@gmail.com>
2021-03-27 12:13:07 +0800

    view template blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('templates._main')
+
+@section('judul', 'Home')
+
+@section('isi')
+    <div class="row">
+        <div class="col-12">
+            <h2 class="mt-3 alert alert-warning">Selamat Pulang </h2>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,37 +10,51 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix' => 'belajar'], function () {
+
+    Route::get('/mahasiswa', function () {
+        echo 'Mahasiswa';
+    });
+
+    Route::get('/ubg/ilkom/mahasiswa', function () {
+        return 'Mahasiswa Ilkom Kampus UBG';
+    });
+
+// routing dengan parameter
+    Route::get('/ubg/ilkom/mahasiswa/{semester}/{nim}', function ($semester, $nim) {
+        return 'Mahasiswa Ilkom Kampus UBG Semester : ' . $semester . ' - NIM : ' . $nim;
+    });
+
+    Route::redirect('/login', '/masuk');
+
+//Group route
+    Route::group(['prefix' => '/admin'], function () {
+        Route::get('/mahasiswa', function () {
+            return 'Admin Mahasiswa';
+        });
+        Route::get('/dosen', function () {
+            return 'Admin Dosen';
+        });
+        Route::get('/pegawai', function () {
+            return 'Admin Pegawai';
+        });
+    });
+});
+
 
 // routing default
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/mahasiswa', function () {
-    echo 'Mahasiswa';
+Route::get('/home', function () {
+    return view('index');
 });
 
-Route::get('/ubg/ilkom/mahasiswa', function () {
-    return 'Mahasiswa Ilkom Kampus UBG';
+Route::get('/kontak/{hp?}', function ($hp) {
+    return view('kontak', ['handphone' => $hp]);
 });
 
-// routing dengan parameter
-Route::get('/ubg/ilkom/mahasiswa/{semester}/{nim}', function ($semester, $nim) {
-    return 'Mahasiswa Ilkom Kampus UBG Semester : ' . $semester . ' - NIM : ' . $nim;
+Route::get('/tentang-kami', function () {
+    return view('tentang');
 });
-
-Route::redirect('/login', '/masuk');
-
-//Group route
-Route::group(['prefix' => '/admin'], function () {
-    Route::get('/mahasiswa', function () {
-        return 'Admin Mahasiswa';
-    });
-    Route::get('/dosen', function () {
-        return 'Admin Dosen';
-    });
-    Route::get('/pegawai', function () {
-        return 'Admin Pegawai';
-    });
-});
-
